e13ee5978abc19f219dafc2d47f1f7e2f8cba456
tomasz <tomaszkacperkulig@gmail.com>
2022-03-23 13:39:13 +0100

    working

--- aave_led_working.ino
+++ aave_led_working.ino
@@ -7,6 +7,9 @@ const char* password = "***REMOVED***";
 
 WiFiServer server(80);
 
+String header;
+String chain = "Avalanche";
+
 #define AAVE_PIN 4
 #define AAVE_NUMPIXELS 7
 #define DELAYVAL 35
@@ -62,26 +65,125 @@ void setup() {
 
   Serial.begin(9600);
 
-  //  WiFi.begin(ssid, password);
-  //  Serial.print("Connecting to ");
-  //  Serial.println(ssid);
-  //  while (WiFi.status() != WL_CONNECTED) {
-  //    delay(500);
-  //    Serial.print(".");
-  //  }
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
 
   setupAaveColors();
   setupIndicatorColors();
-  indicatorDisplay(16);
+  //indicatorDisplay(12);
 }
 
 void loop() {
   displayAaveColors();
+
+  WiFiClient client = server.available(); // Listen for incoming clients
+  if (client) { // If a new client connects,
+    String currentLine = ""; // make a String to hold incoming data from the client
+    while (client.connected()) { // loop while the client's connected
+      if (client.available()) { // if there's bytes to read from the client,
+        char c = client.read(); // read a byte, then
+        Serial.write(c); // print it out the serial monitor
+        header += c;
+        if (c == '\n') { // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /green/on") >= 0) {
+              Serial.println("green on");
+              //greenstate = "on";
+              //digitalWrite(greenled, HIGH);
+            } else if (header.indexOf("GET /green/off") >= 0) {
+              Serial.println("green off");
+              //greenstate = "off";
+              //digitalWrite(greenled, LOW);
+            } else if (header.indexOf("GET /red/on") >= 0) {
+              Serial.println("red on");
+              //redstate = "on";
+              //digitalWrite(redled, HIGH);
+            } else if (header.indexOf("GET /red/off") >= 0) {
+              Serial.println("red off");
+              //redstate = "off";
+              //digitalWrite(redled, LOW);
+            }
+
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            // drop down
+            client.println(".dropbtn {background-color: #04AA6D; min-width: 160px; color: white; padding: 16px; font-size: 16px; border: none; }");
+            client.println(".dropdown { position: relative; display: inline-block; }");
+            client.println(".dropdown-content { display: none; position: absolute; background-color: #f1f1f1; min-width: 160px; box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2); z-index: 1; }");
+            client.println(".dropdown-content a { color: black; padding: 12px 16px; text-decoration: none; display: block; }");
+            client.println(".dropdown-content a:hover {background-color: #ddd;}");
+            client.println(".dropdown:hover .dropdown-content {display: block;}");
+            client.println(".dropdown:hover .dropbtn {background-color: #3e8e41;}");
+            client.println("</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>AAVE indicator</h1>");
+            
+            client.println("<div class=\"dropdown\"> <button class=\"dropbtn\">" + chain + "</button> <div class=\"dropdown-content\">");
+            client.println("<a href=\"#\">Avalanche</a> <a href=\"#\">Polygon</a> <a href=\"#\">Ethereum</a> </div> </div>");
+           
+            // Display current state, and ON/OFF buttons for GPIO 4
+            //client.println("<p>red - State " + redstate + "</p>");
+            // If the red LED is off, it displays the ON button
+            if ("" == "off") {
+              client.println("<p><a href=\"/red/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/red/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+
+            client.println("</body></html>");
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') { // if you got anything else but a carriage return character,
+          currentLine += c; // add it to the end of the currentLine
+        }
+      }
+    }
+
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
 }
 
 void indicatorDisplay(int pixels) {
   for (int i = 0; i < pixels; i++) {
-     indicator.setPixelColor(i, indicator_colors[i]);
+    indicator.setPixelColor(i, indicator_colors[i]);
   }
   indicator.show();
 }
@@ -160,7 +262,6 @@ bool changeColors(int pixelNo, Color dest) {
   pixels.show();
 
   bool result = red / divider == dest.red / divider && green / divider == dest.green / divider && blue / divider == dest.blue / divider;
-  Serial.println(result);
   return result;
 }
 
@@ -182,10 +283,4 @@ void changeDestinationColors() {
   colors[3] = colors[2];
   colors[2] = colors[1];
   colors[1] = colors[0];
-  
-  
-  
-  
-  
-  
 }
